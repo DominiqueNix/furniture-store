24241eaf3843810a05dd900eaa4213395754c85b
DominiqueNix <dominiquenix@google.com>
2024-07-03 17:06:55 -0500

    added admin view

--- server/src/main/java/com/furniture_store/FurnitureStoreApplication.java
+++ server/src/main/java/com/furniture_store/FurnitureStoreApplication.java
@@ -3,7 +3,6 @@ package com.furniture_store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableMongoRepositories
--- server/src/main/java/com/furniture_store/Service/ProductService.java
+++ server/src/main/java/com/furniture_store/Service/ProductService.java
@@ -1,15 +1,24 @@
 package com.furniture_store.Service;
 
+import java.util.List;
+
+import com.furniture_store.Model.Product;
+
 public interface ProductService {
     // Get all products
+    List<Product> getAllProducts();
 
     // Get one product
+    Product getProduct();
 
     // Add new product
+    Product saveNewProduct();
 
     // update product
         // Auth protected
+    Product updateProduct();
 
     // Delete Product
         // Auth Protected
+    void deleteProduct();
 }
