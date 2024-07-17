f5e29238099b336d081ea88f0e7c0ea230353703
DominiqueNix <dominiquenix@google.com>
2024-07-17 11:14:26 -0500

    updated application load test

--- server/src/test/java/com/furniture_store/FurnitureStoreApplicationTests.java
+++ server/src/test/java/com/furniture_store/FurnitureStoreApplicationTests.java
@@ -3,7 +3,7 @@ package com.furniture_store;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FurnitureStoreApplication.class)
 class FurnitureStoreApplicationTests {
 
 	@Test
