497bc719a1248c19d9126171917d391165b5d1e6
Dominique216 <raquel_0216@yahoo.com>
2024-07-19 14:03:32 -0500

    added react fetch call to get all products

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,30 @@ import { Landing } from './components/Landing';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AllItems } from './components/AllItems';
 import { Admin } from './components/Admin';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = () => {
+      fetch("http://localhost:8080/products")
+        .then(res => res.json())
+        .then(data =>{ 
+          console.log(data)
+          setItems(data)
+          })
+  }
+
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
   return (
-    // <main>
-    //   <Nav />
-    //   <Landing />  
-    // </main>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}/>
-        <Route path="/items" element={<AllItems />}/>
+        <Route path="/items" element={<AllItems  items={items}/>}/>
         <Route path="/admin" element={<Admin />}/>
       </Routes>
     </BrowserRouter>
