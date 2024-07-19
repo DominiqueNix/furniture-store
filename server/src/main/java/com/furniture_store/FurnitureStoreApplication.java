c4edcc5f734ae310af3f73454714147cdbd3bbef
Dominique216 <raquel_0216@yahoo.com>
2024-07-19 14:09:51 -0500

    added cors configuration

--- server/src/main/java/com/furniture_store/FurnitureStoreApplication.java
+++ server/src/main/java/com/furniture_store/FurnitureStoreApplication.java
@@ -2,10 +2,26 @@ package com.furniture_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class FurnitureStoreApplication {
 	public static void main(String[] args) {SpringApplication.run(FurnitureStoreApplication.class, args);}
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 }
